39688ce3e5bcbdbad66032dad3c8b0d5d4332d14
Kevin Ha <kcha@users.noreply.github.com>
2014-09-16 12:01:15 -0400

    Initial import

---
+++ analysis.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+library(grid)
+library(ggmap)
+library(plyr)
+library(stringr)
+
+duration_to_minutes <- function(dur) {
+  # Convert 0:00:00 time format to minutes
+  t <- str_split(dur, ":")
+  t <- lapply(t, as.numeric)
+  t <- do.call("rbind", t)
+  t <- as.matrix(t)
+  minutes <- 60*t[,1] + t[,2] + t[,3]/60
+  return(minutes)
+}
+
+# Load data
+data <- read.table("data/ride_data.txt", header = TRUE, sep = "\t")
+
+# data <- head(data)
+
+################################################################################
+# Plot frequency of stations on map
+
+# Organize data
+stations <- data.frame(stations = c(as.character(data$Start.Station), 
+                                    as.character(data$End.Station)))
+stations$stations <- paste0(stations$stations, ", Toronto, ON")
+geo <- apply(unique(stations), 1, geocode)
+geo <- data.frame(stations = unique(stations$stations), do.call("rbind", geo))
+geo$stations <- as.character(geo$stations)
+stations <- join(stations, geo)
+# stations.loc <- data.frame(stations, do.call("rbind", geo))
+
+freq <- ddply(stations, .(stations, lon, lat), summarize, N = length(stations))
+  
+# Load map
+city <- get_map("toronto", zoom=14, maptype = "roadmap")
+
+# Plot
+ggmap(city, extent = 'device') + 
+  geom_point(aes(x = lon, y = lat, colour = N, size = N), data = freq) +
+  scale_size_continuous("Number of Stations", range = c(4,12)) +
+  scale_colour_gradient("Number of Visits") + 
+  guides(size = FALSE)
+  
+
---
+++ cleanse_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FILE=data/ride_data.txt
+
+cat $FILE \| \
+    sed 's/Ferry Ramp and Queens Quay W/Harbour Square Park/g' - \| \
+    sed 's/Adelaide St/Adelaide St E/g' - \|
+    sed 's/ \/ /&/g' -
