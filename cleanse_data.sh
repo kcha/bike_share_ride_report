5c428cf97a9583af651f3e84cbafe38351cf66a3
Kevin Ha <kcha@users.noreply.github.com>
2014-11-28 15:32:42 -0500

    Change sed substitution

--- cleanse_data.sh
+++ cleanse_data.sh
@@ -5,4 +5,4 @@ FILE=data/ride_data.txt
 cat $FILE | \
     sed 's/Ferry Ramp and Queens Quay W/Harbour Square Park/g' | \
     sed 's/Adelaide St/Adelaide St E/g' | \
-    sed 's/ \/ /&/g'
+    sed 's/ \/ / and /g'
