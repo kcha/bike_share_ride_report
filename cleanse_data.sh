bca829c3af46b4b14129fe869131a1965b8502a8
Kevin Ha <kcha@users.noreply.github.com>
2014-11-28 15:44:03 -0500

    Move backslash substitution up

--- cleanse_data.sh
+++ cleanse_data.sh
@@ -3,6 +3,6 @@
 FILE=data/ride_data.txt
 
 cat $FILE | \
+    sed 's/ \/ / and /g' | \
     sed 's/Ferry Ramp and Queens Quay W/Harbour Square Park/g' | \
-    sed 's/Adelaide St/Adelaide St E/g' | \
-    sed 's/ \/ / and /g'
+    sed 's/Adelaide St( E)*/Adelaide St E/g'
