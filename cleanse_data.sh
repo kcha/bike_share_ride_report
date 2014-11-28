36659968d2b6684f861b78a7179b73929302c144
Kevin Ha <kcha@users.noreply.github.com>
2014-11-28 14:56:45 -0500

    Fix buggy pipe

--- cleanse_data.sh
+++ cleanse_data.sh
@@ -2,7 +2,7 @@
 
 FILE=data/ride_data.txt
 
-cat $FILE \| \
-    sed 's/Ferry Ramp and Queens Quay W/Harbour Square Park/g' - \| \
-    sed 's/Adelaide St/Adelaide St E/g' - \|
-    sed 's/ \/ /&/g' -
+cat $FILE | \
+    sed 's/Ferry Ramp and Queens Quay W/Harbour Square Park/g' | \
+    sed 's/Adelaide St/Adelaide St E/g' | \
+    sed 's/ \/ /&/g'
