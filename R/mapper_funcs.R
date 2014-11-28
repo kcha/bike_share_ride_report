2de00bc9b097e260c3e3f83628ca232b39a0ee23
Kevin Ha <kcha@users.noreply.github.com>
2014-11-28 18:56:27 -0500

    Add new functions

--- R/mapper_funcs.R
+++ R/mapper_funcs.R
@@ -1,3 +1,7 @@
+library(stringr)
+library(rjson)
+library(assertthat)
+
 cleanse_station_names <- function(stations) {
   # Correct station names that have been moved
   stations <- trim(stations)
@@ -27,4 +31,46 @@ duration_to_minutes <- function(dur) {
   t <- as.matrix(t)
   minutes <- 60*t[,1] + t[,2] + t[,3]/60
   return(round(minutes, digits=2))
+}
+
+get_ride_data <- function(ride_data) {
+  write(paste("Reading ride data from", ride_data), stderr())
+  data <- read.table(input_file, header = TRUE, sep = "\t", stringsAsFactors=FALSE)
+  data$Duration <- duration_to_minutes(data$Duration)
+  data$Start.Station <- cleanse_station_names(data$Start.Station)
+  data$End.Station <- cleanse_station_names(data$End.Station)
+  return(data)
+}
+
+get_bike_share_data <- function(
+  json_url = "http://www.bikesharetoronto.com/stations/json") {
+  # Download Bike Share Toronto station data
+  write(paste("Downloading bike share stations from", json_url), stderr())
+  stations <- fromJSON(file=json_url, method='C')
+  stations <- data.frame(do.call("rbind", stations$stationBeanList), 
+                         stringsAsFactors=FALSE)
+  for (j in 1:ncol(stations)) {
+    if (is.list(stations[,j])) {
+      z <- unlist(stations[,j])
+      if (is.null(z)) {
+        stations[,j] <- rep("NULL", nrow(stations))
+      } else {
+        stations[,j] <- z
+      }
+    }
+  } 
+  stations$stationName <- cleanse_station_names(stations$stationName)
+  return(stations)
+}
+
+generate_random_dates <- function(ndates, start = "2013/01/01 00:00 AM", end = "2014/12/31 12:00 PM") {
+  first <- as.POSIXct(strptime(start, "%Y/%m/%d %H:%M %p"))
+  last <- as.POSIXct(strptime(end, "%Y/%m/%d %H:%M %p"))
+  td <- last - first
+  
+  # first day + random day + random time of day
+  rdates <- first + sample(as.numeric(dt), ndates) * 86400 + 
+    runif(ndates, 1, 86400) 
+  rdates <- sort(rdates)
+  return(rdates)  
 }
