e3129988dfeec014a0f946f0a35efacc1c90ee18
Kevin Ha <kcha@users.noreply.github.com>
2014-11-29 12:24:01 -0500

    Move mapping code to its own function

--- mapper.R
+++ mapper.R
@@ -1,7 +1,5 @@
 #!/usr/bin/env Rscript
 
-library(ggmap)
-library(plyr)
 source("R/mapper_funcs.R")
 
 # Load bike share stations ####
@@ -11,31 +9,11 @@ stations <- get_bike_share_data()
 input_file <- "data/ride_data.txt"
 data <- get_ride_data(input_file)
 
-# Get longitude and latitude coordinates
-starts <- join(data.frame(stationName=data$Start.Station, stringsAsFactors=FALSE), 
-               stations[, c("stationName", "latitude", "longitude")])
-ends <- join(data.frame(stationName=data$End.Station, stringsAsFactors=FALSE),
-             stations[, c("stationName", "latitude", "longitude")])
-assert_that(nrow(starts) == nrow(ends))
-
 # Get frequencies ####
-freq <- rbind(data.frame(ddply(starts, .(stationName, longitude, latitude), 
-                               summarize, N = length(stationName)),
-                         type = "Start"),
-              data.frame(ddply(ends, .(stationName, longitude, latitude), 
-                               summarize, N = length(stationName)),
-                         type = "End"))
+freq <- calculate_station_frequencies(data, stations)
   
-# Load map ####
-# city <- get_map("downtown toronto", zoom=13, maptype = "roadmap")
-city <- get_map(calculate_bbox(freq$longitude, freq$latitude), zoom=14, 
-                maptype = "roadmap")
-
 # Plot map ####
-gp1 <- ggmap(city, extent = 'device') + 
-  geom_point(aes(x = longitude, y = latitude, colour = N, size = N), data = freq) + 
-  scale_size_continuous("", range = c(4,12)) +
-  scale_colour_gradient("Number of Visits", low="orange", high="red") + 
-  facet_wrap(~ type, ncol = 2) + 
-  ggtitle(input_file) 
+gp1 <- map_stations(freq) + 
+  facet_wrap(~ type, ncol = 2) +
+  ggtitle(input_file)
 print(gp1)
